84169b3feb92e7d78139959851fc2d01c5a4bafb
Umut_Ciloglu <umutciloglu06@gmail.com>
2022-05-07 15:55:47 +0300

    add equipment model/dao

--- src/DB/DAO/ActivityDAO.java
+++ src/DB/DAO/ActivityDAO.java
@@ -7,13 +7,13 @@ import DB.DbContext;
 public class ActivityDAO {
     public void addIndividualActivity(IndividualActivity individualActivity){
         String sql = "call addIndActivity('" + individualActivity.getID() + "','"+ individualActivity.getName() + "','" + individualActivity.getLink() +"','" +
-                individualActivity.getDate() + "','" + individualActivity.getHour() + "','" + individualActivity.getAge() + "';";
+                individualActivity.getDate() + "','" + individualActivity.getHour() + "','" + individualActivity.getAge() + "');";
         DbContext.ExecuteQuery(sql);
     }
 
     public void addMassActivity(MassActivity massActivity){
         String sql = "call addIndActivity('" + massActivity.getID() + "','"+ massActivity.getName() + "','" + massActivity.getLink() +"','" +
-                massActivity.getDate() + "','" + massActivity.getHour() + "','" + massActivity.getType() + "','" + massActivity.getCapacity() + "';";
+                massActivity.getDate() + "','" + massActivity.getHour() + "','" + massActivity.getType() + "','" + massActivity.getCapacity() + "');";
         DbContext.ExecuteQuery(sql);
     }
 
